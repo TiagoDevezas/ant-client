5fcd76ffe09f767987b6833c15181447e3298144
Tiago Devezas <tiagodevezas@gmail.com>
2016-05-01 00:28:41 +0100

    fetch pictures from student/employee codes

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 // import {router} from '../main'
 
-const API_URL = 'http://ant.fe.up.pt/'
+// const API_URL = 'http://ant.fe.up.pt/'
+// const SEARCH_URL = API_URL + 'search'
+// const JSON_SEARCH_URL = SEARCH_URL + '.json'
+// const METADATA_SEARCH_URL = SEARCH_URL + '/decorator/metadata.json'
+
+const API_URL = 'http://maltese.fe.up.pt/api/'
 const SEARCH_URL = API_URL + 'search'
-const JSON_SEARCH_URL = SEARCH_URL + '.json'
-const METADATA_SEARCH_URL = SEARCH_URL + '/decorator/metadata.json'
+const JSON_SEARCH_URL = SEARCH_URL
+const METADATA_SEARCH_URL = SEARCH_URL + '/decorator/metadata'
 
 const store = Object.create(null)
 
 export default store
 
-var t0
+let t0
 
 store.getEntities = (context, startPage) => {
   const query = context.$route.query.q
@@ -17,8 +22,8 @@ store.getEntities = (context, startPage) => {
   return context.$http({url: JSON_SEARCH_URL, method: 'GET', params: {q: query, start: start}})
 }
 store.getEntityMetada = (context, item) => {
-  let entity = item.uri.split('#')[1]
-  let type = item.type.uri.split('#')[1]
+  const entity = item.uri.split('#')[1]
+  const type = item.type.uri.split('#')[1]
   return context.$http({url: METADATA_SEARCH_URL, method: 'GET', params: {entity: entity, type: type}})
 }
 
@@ -26,10 +31,10 @@ store.getEntitiesWithMetadata = (context, startPage) => {
   t0 = performance.now()
   return store.getEntities(context, startPage)
     .then(response => {
-      let t1 = performance.now()
-      let timeToSearch = ((t1 - t0) * 0.001).toFixed(2)
+      const t1 = performance.now()
+      const timeToSearch = ((t1 - t0) * 0.001).toFixed(2)
       context.$set('timeToSearch', timeToSearch)
-      let data = response.data.data
+      const data = response.data.data
       context.$set('data.metadata', response.data.metadata)
       data.forEach(d => {
         store.getEntityMetada(context, d)
