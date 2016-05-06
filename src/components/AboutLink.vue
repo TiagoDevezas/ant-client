f09f6b40deebade668fc821db42dc68ccb2e8476
tiagodevezas <tiagodevezas@gmail.com>
2016-05-07 00:16:31 +0100

    add about view and link component

---
+++ src/components/AboutLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="floating-link">
+    <a href="#!{{ linkPath }}">{{ linkText }}</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['linkText', 'linkPath']
+  }
+</script>
+
+<style>
+  #floating-link {
+    position: absolute;
+    right: 0;
+    top: 0;
+    background-color: #E5E5E5;
+    z-index: 100;
+  }
+
+  #floating-link:hover {
+    background-color: #D8D8D8;
+  }
+
+  #floating-link a {
+    padding: 10px 25px;
+    text-decoration: none;
+    color: #555555;
+    text-transform: uppercase;
+    font-size: .8em;
+    line-height: 1.2;
+    display: inline-block;
+  }
+</style>
---
+++ src/components/AboutView.vue
@@ -0,0 +1,100 @@
+<template>
+  <about-link link-text="Início" link-path="/"></about-link>
+  <div id="about-wrapper">
+    <h1>Sobre</h1>
+    <p>O objetivo do ANT é o de disponibilizar uma plataforma educativa e para Investigação & Desenvolvimento, na área da pesquisa orientada a entidades.</p>
+    <p>Atualmente, é utilizado o sistema de informação <a href="http://sigarra.up.pt/" target="_blank">SIGARRA</a> como caso de estudo central. A longo prazo, o objetivo será o de estender o suporte a coleções mais genéricas, tornando o sistema mais modular e universal. A ideia é a de fomentar a execução de experiências académicas nas várias subáreas de investigação, com suporte numa plataforma base que permitirá acelerar novas contribuições, de forma mais focada e sem o esforço inicial de preparação. Permitir-se-á assim não só integrar rapidamente qualquer desenvolvimento adicional aplicado ao nosso caso de estudo em particular, mas também executar experiências científicas e sessões educacionais diretamente suportadas pela nossa plataforma.</p>
+
+    <h1>Colaboração</h1>
+    <p>Se pertences à Universidade do Porto e pretendes colaborar no projeto ANT para desenvolver competências na área da recuperação de informação, em particular da pesquisa orientada a entidades, entra em contacto connosco.</p>
+
+    <p>Podes fazê-lo via <a href="/contact">e-mail</a>, ou pessoalmente no Laboratório de Sistemas de Informação, que fica na sala <a href="#!/search?q=i123">i123</a> do <a href="#!/search?q=departamento+de+engenharia+inform%C3%A1tica">Departamento de Engenharia Informática</a> — pergunta pelo <a href="#!/search?q=jos%C3%A9+devezas+funcion%C3%A1rio">José Devezas</a> ou pelo <a href="#!/search?q=s%C3%A9rgio+nunes+funcion%C3%A1rio">Sérgio Nunes</a>.</p>
+
+    <h1>Contactos</h1>
+    <address>
+      <strong>Sérgio Nunes</strong><br>
+      Departamento de Engenharia Informática,<br>
+      Faculdade de Engenharia, Universidade do Porto<br>
+      <a href="mailto:ssn@fe.up.pt">ssn@fe.up.pt</a>
+    </address>
+    <address>
+      <strong>José Devezas *</strong><br>
+      Laboratório de Sistemas de Informação,<br>
+      Faculdade de Engenharia, Universidade do Porto<br>
+      <a href="mailto:jld@fe.up.pt">jld@fe.up.pt</a>
+    </address>
+    <p class="smaller">
+      * Contacto para envio de sugestões, bugs ou outro feedback.
+    </p>
+
+    <h1>Agradecimentos</h1>
+    <dl>
+      <dt><a href="#!/search?q=tiago+devezas">Tiago Devezas</a></dt>
+      <dd>Front-End Development</dd>
+      <dt><a href="#!/search?q=no%C3%A9mia+moreira">Noémia Moreira</a></dt>
+      <dd>UX Analyst</dd>
+    </dl>
+    <dl>
+      <dt><a href="#!/search?q=ricardo+carvalho+amorim">Ricardo Amorim</a></dt>
+      <dd>Android Development</dd>
+      <dt><a href="#!/search?q=ana+de+lurdes+rocha+reis">Ana Rocha Reis</a></dt>
+      <dd>Android Design</dd>
+    </dl>
+
+    <p><em><a href="http://ant.fe.up.pt/dev">Versão de desenvolvimento</a></em></p>
+  </div>
+</template>
+
+<script>
+  import AboutLink from './AboutLink'
+
+  export default {
+    components: {
+      AboutLink
+    },
+    ready () {
+      document.title = 'Sobre - ANT'
+    }
+  }
+</script>
+
+<style>
+  #about-wrapper {
+    padding-top: 0.5em;
+    position: relative;
+    padding-left: 94px;
+    max-width: 800px;
+    margin-bottom: 50px;
+  }
+
+  #about-wrapper h1 {
+    font-size: 20px;
+    line-height: 1;
+    color: #4C4C4C;
+    margin: 40px 0;
+    /*text-transform: uppercase;*/
+  }
+
+  #about-wrapper address {
+    margin-bottom: 20px;
+  }
+
+  #about-wrapper dt {
+  }
+
+  #about-wrapper dd {
+    margin-left: 0px;
+    margin-bottom: 15px;
+  }
+
+  #about-wrapper p {
+    font-size: 15px;
+    line-height: 1.3;
+  }
+
+  #about-wrapper a {
+    text-decoration: none;
+    color: #de5833;
+  }
+  
+</style>
