85f9932b8a7dbfcf9424173505b9c61383422ae0
tiagodevezas <tiagodevezas@gmail.com>
2017-11-09 14:34:48 +0000

    suggest unfiltered query when then aren't results

--- src/components/ResultEmpty.vue
+++ src/components/ResultEmpty.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="result-empty">
   <p style="font-size: 18px;">Nenhum documento encontrado para a pesquisa actual.</p>
-  <p>Por favor verifique se nenhuma palavra contém erros ortográficos, ou tente utilizar outras palavras-chave.</p> 
+  <!-- <p>Por favor verifique se nenhuma palavra contém erros ortográficos, ou tente utilizar outras palavras-chave.</p> -->
+  <p>Por favor verifique as palavras introduzidas, ou tente a pesquisa sem filtros: <a href="search?q={{$route.query.q}}">{{$route.query.q}}</a>
 </div>
 </template>
 
