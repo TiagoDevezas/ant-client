81a0dffcc3697d69fc2a1ffd877a2faa79781d65
tiagodevezas <tiagodevezas@gmail.com>
2017-11-23 15:40:41 +0000

    fix bug when there are no results and no filters are selected

--- src/components/ResultEmpty.vue
+++ src/components/ResultEmpty.vue
@@ -1,12 +1,23 @@
 <template>
 <div class="result-empty">
   <p style="font-size: 18px;">Nenhum documento encontrado para a pesquisa actual.</p>
-  <!-- <p>Por favor verifique se nenhuma palavra contém erros ortográficos, ou tente utilizar outras palavras-chave.</p> -->
-  <p>Por favor verifique as palavras introduzidas, ou tente a pesquisa sem filtros: <a href="search?q={{$route.query.q}}">{{$route.query.q}}</a>
+  <p v-if="hasQueryAndNoFilters">Por favor verifique se nenhuma palavra contém erros ortográficos, ou tente utilizar outras palavras-chave.</p>
+
+  <p v-if="!hasQueryAndNoFilters">Por favor verifique as palavras introduzidas, ou tente a pesquisa sem filtros: <a href="search?q={{$route.query.q}}">{{$route.query.q}}</a>
 </div>
 </template>
 
 <script>
+  export default {
+    computed: {
+      hasQueryAndNoFilters: function () {
+        if (this.$route.query.q && Object.keys(this.$route.query).length === 1) {
+          return true
+        }
+        return false
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
